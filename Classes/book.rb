47a63cb2ba27cc83efbe767e7baf0b4f2751002a
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-08-07 22:09:17 +0600

    Add methods

--- Classes/book.rb
+++ Classes/book.rb
@@ -1,5 +1,18 @@
-class Book
-  def initialize
-    
+require_relative 'item'
+
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publisher, cover_state)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    if @cover_state == 'bad' || super
+      true
+    else
+      false
+    end
   end
 end
