e7df21c57dc7485a248a8d02f43038a44c8af826
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-08-07 22:09:26 +0600

    Add methods

--- Classes/label.rb
+++ Classes/label.rb
@@ -1,5 +1,14 @@
-class label
-  def initialize
-    
+require_relative 'item'
+
+class Label < Item
+  def initialize(id,title,color,items)
+    @id = Random.rand(1..1000)
+    @title = title
+    @color = color
+    items = []
+  end
+
+  def add_item(item)
+    @items.push(item)
   end
 end
