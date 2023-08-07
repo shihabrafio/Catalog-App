169fc7c46514b9ff8c349774cd4e253bee31143f
Gakii Beryl <gakiiberyl@gmail.com>
2023-08-07 12:04:17 +0000

    Update menu options in Options class

---
+++ Classes/options.rb
@@ -0,0 +1,16 @@
+class Options
+  def choices
+    puts 'WELCOME TO CATALOG OF MY THINGS APP 💁🏿'
+    puts 'Choose an option by entering its number'
+    puts '1. List all games'
+    puts '2. List all books  📚'
+    puts '3. List all music albums 🎶'
+    puts '4. List all genres'
+    puts '5. List all labels'
+    puts '6. List all authors'
+    puts '7. Add a book'
+    puts '8. Add a music album'
+    puts '9. Add a game'
+    puts '0. Quit'
+  end
+end
