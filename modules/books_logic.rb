7bc36e7beee6e0d27d0b4556e3730b82223d2ea5
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-08-08 16:25:28 +0600

    Fix Linter Error 2

--- modules/books_logic.rb
+++ modules/books_logic.rb
@@ -8,17 +8,18 @@ module BookModule
     if File.exist?(file)
       JSON.parse(File.read(file)).each do |book|
         data.push(Book.new(book['publisher'], book['cover_state'], book['publish_date']))
-    end
+      end
     else
-        File.write(file,[])
+      File.write(file, [])
     end
   end
 
   def write_book
     data = []
     @books.each do |book|
-        data.push({ 'publisher' => book.publisher, 'cover_state' => book.cover_state, 'publish_date' => book.publish_date })
-  end
-  File.write('./json_files/books.json', JSON.pretty_generate(data))
+      data.push({ 'publisher' => book.publisher, 'cover_state' => book.cover_state,
+                  'publish_date' => book.publish_date })
+    end
+    File.write('./json_files/books.json', JSON.pretty_generate(data))
   end
 end
