6b88e356c7051f392813647c19a7074f79dc655c
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-08-08 16:21:12 +0600

    Add write_book method

--- modules/books_logic.rb
+++ modules/books_logic.rb
@@ -1,13 +1,24 @@
+require 'json'
+require_relative '../classes/book'
+
 module BookModule
-  def create_book(_book)
+  def read_book
     data = []
     file = './json_files/books.json'
     if File.exist?(file)
       JSON.parse(File.read(file)).each do |book|
         data.push(Book.new(book['publisher'], book['cover_state'], book['publish_date']))
-      end
+    end
     else
-      File.write(file, [])
+        File.write(file,[])
     end
   end
+
+  def write_book
+    data = []
+    @books.each do |book|
+        data.push({ 'publisher' => book.publisher, 'cover_state' => book.cover_state, 'publish_date' => book.publish_date })
+  end
+  File.write('./json_files/books.json', JSON.pretty_generate(data))
+end
 end
