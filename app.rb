f142e1e89776bebb81cb572baa666ee8abec2647
Gakii Beryl <gakiiberyl@gmail.com>
2023-08-09 19:44:36 +0000

    Reducing number of lines in app file

--- app.rb
+++ app.rb
@@ -8,9 +8,9 @@ require './modules/book_logic'
 require './modules/label_logic'
 require './Classes/music'
 require 'json'
-require './modules/load-genres'
-require './modules/load-music'
-
+require './modules/load_genres'
+require './modules/load_music'
+require './modules/add_music'
 class App
   puts "Welcome to The Content Hub!\n\n"
   include BookModule
@@ -156,42 +156,6 @@ class App
     book = Book.new(author, publisher, cover_state, publish_date)
     @books.push(book)
     @labels.push(Label.new(title, color))
-  end
-
-  def add_music_album
-    puts 'Creating a music album... Add details below.'
-    print 'Album Name: '
-    name = gets.chomp
-
-    print 'Publish Date: '
-    publish_date = gets.chomp
-
-    print 'Cover State (good, bad, etc.): '
-    cover_state = gets.chomp
-
-    print 'On Spotify (true/false): '
-    on_spotify = gets.chomp.downcase == 'true'
-
-    print 'Archived (true/false): '
-    archived = gets.chomp.downcase == 'true'
-
-    album = MusicAlbum.new(name, publish_date, cover_state, on_spotify, archived: archived)
-    @albums << album
-
-    puts 'Music album created successfully'
-
-    puts 'What genre is the above music album?'
-    genre_name = gets.chomp
-
-    existing_genre = @genres.find { |g| g.name == genre_name }
-
-    if existing_genre
-      album.genre = existing_genre
-    else
-      new_genre = Genre.new(genre_name)
-      @genres << new_genre
-      album.genre = new_genre
-    end
   end
 
   def save_genres_to_json
