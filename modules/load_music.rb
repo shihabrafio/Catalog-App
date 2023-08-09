0d8b5e01a0f3a143d8c782719114f627f1814a85
Gakii Beryl <gakiiberyl@gmail.com>
2023-08-09 19:56:59 +0000

    Fixing linters error

--- modules/load_music.rb
+++ modules/load_music.rb
@@ -1,10 +1,10 @@
 def load_albums_from_json
   if File.exist?('./json_files/albums.json')
     albums_data = JSON.parse(File.read('./json_files/albums.json'))
-    albums_data.map { |data|
+    albums_data.map do |data|
       MusicAlbum.new(data['name'], data['publish_date'], data['cover_state'], data['on_spotify'],
                      archived: data['archived'])
-    }
+    end
   else
     []
   end
