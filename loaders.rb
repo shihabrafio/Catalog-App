001846383458fd690b123614d129004439e94f2d
Gakii Beryl <gakiiberyl@gmail.com>
2023-08-09 19:16:08 +0000

    Fixing linters error

--- loaders.rb
+++ loaders.rb
@@ -18,5 +18,4 @@ class Loader
       games = []
     end
   end
-
 end
--- modules/load-music.rb
+++ modules/load-music.rb
@@ -1,7 +1,10 @@
 def load_albums_from_json
   if File.exist?('./json_files/albums.json')
     albums_data = JSON.parse(File.read('./json_files/albums.json'))
-    albums_data.map { |data| MusicAlbum.new(data['name'], data['publish_date'], data['cover_state'], data['on_spotify'], archived: data['archived']) }
+    albums_data.map { |data|
+      MusicAlbum.new(data['name'], data['publish_date'], data['cover_state'], data['on_spotify'],
+                     archived: data['archived'])
+    }
   else
     []
   end
