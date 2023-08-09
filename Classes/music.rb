5264451e86624f4920908f1c4180f4021d7fc07a
berylcl <gakiiberyl@gmail.com>
2023-08-09 18:16:23 +0100

    Modifying arguments in music class "
    "
    
    q

--- Classes/music.rb
+++ Classes/music.rb
@@ -2,12 +2,14 @@ class MusicAlbum < Item
   attr_accessor :cover_state, :on_spotify
 
   def initialize(name, publish_date, cover_state, on_spotify, archived: false)
-    super(name, publish_date, archived: archived)
+    super(publish_date, archived: archived)
+    @id = Random.rand(1..1000)
+    @name = name
     @cover_state = cover_state
     @on_spotify = on_spotify
   end
 
   def can_be_archived?
-    super && on_spotify == true
+    super || @cover_state == "bad"
   end
 end
